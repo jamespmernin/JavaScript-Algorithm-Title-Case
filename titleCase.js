e89bd58051a09b007892925a8fca56f16bac3dae
James Mernin <jamespmernin@gmail.com>
2020-10-26 17:58:55 -0400

    Algorithm solved

--- titleCase.js
+++ titleCase.js
@@ -1,7 +1,7 @@
-// TITLE CASE
-
 const titleCase = str => {
-
+  return str.replace(/\w\S*/g, function (txt) { // The regular expression parses non-letter characters correctly
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); // This guarantees that the first letter is uppercase and all subsequent letters are lowercase
+  });
 }
 
 console.log(titleCase("I'm a little tea pot"))
